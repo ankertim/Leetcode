8022b43e971f5ca5617520e5c30c01eed3d943ca
ankertim <yyho.swe@gmail.com>
2022-06-20 16:03:52 +0800

    update 9. Palindrome Number.java

--- Java/9. Palindrome Number.java
+++ Java/9. Palindrome Number.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if (x < 0) return false;
 		int intput = x;
         int temp, revert = 0;
-        while (intput > 0) {
+        while (intput >= 10) {
             revert *= 10;
             revert += intput%10;
             intput /= 10;
         }
-        return revert == x;
+        return revert == x/10 && intput == x%10;
     }
 }
